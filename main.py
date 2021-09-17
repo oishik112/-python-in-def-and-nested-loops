e81162b2191aa78699a02ccf38184ab28352fd37
OISHIK DUTTA <85332693+oishik112@users.noreply.github.com>
2021-09-17 08:51:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+# Multification table using nested loops
+
+num = 8
+
+# Iterate 10 items form i = 1 to 10
+
+for i in range(1, 11):
+  print(num, 'X' , i, '=', num*i )
+
+ch = input("Enter a character: ")
+
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print(ch, "is a Vowel")
+else:
+    print(ch, "is a Consonant")
+
+ch = input("enter a character: ")
+
+if (ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='I' or ch=='i' or ch=='O' or ch== 'o' or ch=='U' or ch=='u'):
+
+    print(ch, "is a Vowel " )
+else:
+    print(ch, "is a Consonant")
+
+
+# Program make a simple calculator
+
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
+    else:
+        print("Invalid Input")
